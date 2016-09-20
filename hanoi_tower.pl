01da41fd7f4086eb3bfe2446931a96687ed5c992
Lucas <lucasrrt@gmail.com>
2016-09-20 10:21:47 -0300

    Mudanças a serem submetidas: 	renamed:    q.pl -> functions.pl 	new file:   hanoi_tower.pl

---
+++ hanoi_tower.pl
@@ -0,0 +1,15 @@
+hanoi(N):-move(N,1,2,3).
+
+move(0,_,_,_):-!.
+
+move(N,A,B,C):-
+	M is N-1,
+	move(M,A,C,B),
+	inform(A,B),
+	move(M,C,B,A).
+
+inform(X,Y):-
+	write('mova um disco do pino '),
+	write(X),
+	write(' para o pino '),
+	write(Y), nl.
